5ce6922660eab6ccac86884e738c7f5dfb08b3d0
MaekTec <markus.kaeppeler@yahoo.de>
2021-06-26 19:49:42 +0200

    fix CIFAR10Custom.py targets
    Signed-off-by: MaekTec <markus.kaeppeler@yahoo.de>

--- data/CIFAR10Custom.py
+++ data/CIFAR10Custom.py
@@ -42,8 +42,10 @@ class CIFAR10Custom(torchvision.datasets.CIFAR10):
             trainset_size = len(self.data) - valset_size
             if train:
                 self.data = self.data[valset_size:]
+                self.targets = self.targets[valset_size:]
             if val:
                 self.data = self.data[:valset_size]
+                self.targets = self.targets[:valset_size]
 
     @staticmethod
     def mean():
