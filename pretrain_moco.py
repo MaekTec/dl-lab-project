09f20837c28e672471fc0d6bb56c44d461971b9e
MaekTec <markus.kaeppeler@yahoo.de>
2021-07-12 18:04:26 +0200

    moco: fix validate
    Signed-off-by: MaekTec <markus.kaeppeler@yahoo.de>

--- pretrain_moco.py
+++ pretrain_moco.py
@@ -37,7 +37,7 @@ def parse_arguments():
     parser.add_argument('--output-root', type=str, default='results')
     parser.add_argument('--lr', type=float, default=0.0002, help='learning rate')
     parser.add_argument('--weight-decay', type=float, default=0.0, help='weight decay')
-    parser.add_argument('--bs', type=int, default=128, help='batch_size')
+    parser.add_argument('--bs', type=int, default=256, help='batch_size')
     parser.add_argument('--epochs', type=int, default=15, help='epochs')
     parser.add_argument('--image-size', type=int, default=64, help='size of image')
     parser.add_argument("--resnet", type=str2bool, nargs='?',
@@ -99,7 +99,7 @@ def main(args):
     for epoch in range(args.epochs):
         logger.info("Epoch {}".format(epoch))
         train_loss, train_acc = train(train_loader, model, criterion, optimizer, scheduler, epoch)
-        val_loss, val_acc = validate(val_loader, model, epoch)
+        val_loss, val_acc = validate(val_loader, model, criterion, epoch)
 
         logger.info('Training loss: {}'.format(train_loss))
         logger.info('Training accuracy: {}'.format(train_acc))
