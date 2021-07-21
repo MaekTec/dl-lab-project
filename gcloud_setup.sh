a38b786bfd43e39a39bba455bb6a1dcf6f6913d0
MaekTec <markus.kaeppeler@yahoo.de>
2021-07-21 23:58:31 +0200

    update run_all.sh and gcloud_setup.sh
    Signed-off-by: MaekTec <markus.kaeppeler@yahoo.de>

--- gcloud_setup.sh
+++ gcloud_setup.sh
@@ -4,3 +4,5 @@ git checkout --track origin/markus
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 test_imports.py
+chmod +x run_all.sh
+./run_all.sh > run_all_log.txt 2>run_all_error.txt &
