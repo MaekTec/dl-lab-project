11cb8cab87d3ee09cd2a819936e5c004b4d63d5a
MaekTec <markus.kaeppeler@yahoo.de>
2021-07-21 23:41:51 +0200

    add test_imports.py and gcloud_setup.sh
    Signed-off-by: MaekTec <markus.kaeppeler@yahoo.de>

---
+++ gcloud_setup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/MaekTec/dl-lab-project.git
+cd dl-lab-project/
+git checkout --track origin/markus
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
