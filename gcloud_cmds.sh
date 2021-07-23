0b61480bd42924a3764e2cd2d3a48af6f7bf29cb
MaekTec <markus.kaeppeler@yahoo.de>
2021-07-23 11:20:14 +0200

    update gcloud_cmds.sh
    Signed-off-by: MaekTec <markus.kaeppeler@yahoo.de>

--- gcloud_cmds.sh
+++ gcloud_cmds.sh
@@ -1,3 +1,4 @@
+# Setup
 git clone https://github.com/MaekTec/dl-lab-project.git
 cd dl-lab-project/
 git checkout --track origin/markus
@@ -5,8 +6,13 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 test_imports.py
 chmod +x run_all.sh
+
+# Run
 ./run_all.sh &> run_all_log.txt &
 disown -h # or -a
 # 30536
 # 2055
+
+# Copy from vm
 zip -r dl-lab-project-glcoud.zip dl-lab-project
+gcloud compute scp vm-instance:/home/ma_kaeppeler/dl-lab-project-glcoud.zip dl-lab-project-glcoud.zip --project contrastive-ss
