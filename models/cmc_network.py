4fbbd51105ed67a00e88115cf8b0f1d6bcb41769
Stanley George <stanley.saji@gmail.com>
2021-07-26 19:14:27 +0200

    added non-linearity

--- models/cmc_network.py
+++ models/cmc_network.py
@@ -5,11 +5,13 @@ class CMCLinearClassifier(nn.Module):
     def __init__(self, model, encoder_dim):
         super().__init__()
         self.transformer = model
-        self.fc1 = nn.Linear(in_features=encoder_dim*2, out_features=10)
+        self.fc1 = nn.Linear(in_features=encoder_dim*2, out_features=encoder_dim)
+        self.fc2 = nn.Linear(in_features=encoder_dim, out_features=10)
 
     def forward(self, x):
         output_l, output_ab = self.transformer(x)
         output = torch.cat((output_l.detach(),output_ab.detach()),dim=1)
-        output = self.fc1(output)
+        output = nn.ReLU(self.fc1(output))
+        output = self.fc2(output)
         return output
 
